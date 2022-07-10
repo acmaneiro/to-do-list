0e91cef837da6be742e0ea0c64412572c49d234f
Aura <aura.maneiro@agsw.es>
2022-07-10 23:49:50 +0200

    todo list

--- src/index.js
+++ src/index.js
@@ -4,14 +4,3 @@ import { createTodoHtml } from './js/componentes';
 
 const todos = [];
 export const todoList = new ToDoList();
-
-const tarea = new Todo('aprender javascript');
-const tarea2 = new Todo('comprar un unicornio');
-todoList.newTodo(tarea);
-todoList.newTodo(tarea2);
-console.log({todoList})
-
-tarea.done = true;
-createTodoHtml(tarea);
-
-createTodoHtml(tarea2);
