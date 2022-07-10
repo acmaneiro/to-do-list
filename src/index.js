af49c0afefa43d9b0fae48e7731312e0519b1ccf
Aura <aura.maneiro@agsw.es>
2022-07-10 23:42:52 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import { buildTitle } from './js/componentes';
 import './styles.css';
-import * as bootstrap from 'bootstrap';
+import { ToDoList, Todo  } from './classes/index';
+import { createTodoHtml } from './js/componentes';
 
+const todos = [];
+export const todoList = new ToDoList();
 
-buildTitle ();
+const tarea = new Todo('aprender javascript');
+const tarea2 = new Todo('comprar un unicornio');
+todoList.newTodo(tarea);
+todoList.newTodo(tarea2);
+console.log({todoList})
 
+tarea.done = true;
+createTodoHtml(tarea);
 
-
-
+createTodoHtml(tarea2);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,12 +1,47 @@
+import { Todo } from "../classes";
+import { todoList } from "../index";
 
-import '../css/componentes.css';
-import webpacklogo from '../assets/img/webpack-logo.png';
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
 
-export const buildTitle = () => {
-    // const h1 = document.createElement('h1');
-    // h1.innerText = 'Webpack starter project';
-    // document.body.append(h1);
-    //  const img = document.createElement('img');
-    //  img.src = webpacklogo;
-    //  document.body.append(img);
+export const createTodoHtml = ( todo ) => {
+    const htmlToDo = `
+    <li class="${todo.done ? 'completed' :''}" data-id="${todo.id}">
+						<div class="view">
+							<input class="toggle" type="checkbox" ${todo.done ? 'checked': ''}>
+							<label>${ todo.tarea}</label>
+							<button class="destroy"></button>
+						</div>
+						<input class="edit" value="Create a TodoMVC template">
+					</li> `;
+    const div = document.createElement('div');
+    div.innerHTML = htmlToDo;
+    divTodoList.append(div.firstElementChild);
+    return div;
 }
+
+// Events
+
+txtInput.addEventListener('keyup', ( event ) => {
+    if (event.keyCode === 13 && txtInput.value.length > 0){
+        const newTodo = new Todo(txtInput.value)
+        todoList.newTodo(newTodo);
+        createTodoHtml(newTodo);
+        txtInput.value = '';
+    }
+})
+
+divTodoList.addEventListener('click', (event) => {
+    const elementName = event.target.localName;
+    const todoElement = event.target.parentElement?.parentElement;
+    const todoId = todoElement.getAttribute('data-id');
+
+    if (elementName.includes('input')) {
+        todoList.toggleTodo(todoId);
+        todoElement.classList.toggle('completed');
+    }else if (elementName.includes('button')){
+        todoList.deleteTodo(todoId);
+        divTodoList.removeChild( todoElement );
+    }
+    console.log(todoList);
+});
